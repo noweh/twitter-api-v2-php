cc39ca8161a094b5967251c8f15439f6d6fdb25e
Cibles <19661971+cibles@users.noreply.github.com>
2023-05-09 19:32:41 +0200

    Add max_results to UserFollows.php
    Add maxResults function in UserFollows class

--- src/UserFollows.php
+++ src/UserFollows.php
@@ -9,6 +9,9 @@ namespace Noweh\TwitterApi;
  */
 class UserFollows extends AbstractController
 {
+    /** @var int $maxResults */
+    private int $maxResults;
+
     /**
      * @param array<int, string> $settings
      * @throws \Exception
@@ -64,6 +67,19 @@ class UserFollows extends AbstractController
     }
 
     /**
+     * The maximum number of search results to be returned by a request.
+     * A number between 1 and 1000.
+     * By default, a request response will return 100 results.
+     * @param int $number
+     * @return $this
+     */
+    public function addMaxResults(int $number): UserFollows
+    {
+        $this->maxResults = $number;
+        return $this;
+    }
+
+    /**
      * Retrieve Endpoint value and rebuilt it with the expected parameters
      * @return string the URL for the request.
      * @throws \Exception
@@ -71,10 +87,19 @@ class UserFollows extends AbstractController
     protected function constructEndpoint(): string
     {
         $endpoint = parent::constructEndpoint();
+
+        if (!empty($this->maxResults)) {
+            $this->query_string['max_results'] = $this->maxResults;
+        }
+
         if (! is_null($this->next_page_token)) {
             $this->query_string['pagination_token'] = $this->next_page_token;
+        }
+
+        if (!empty($this->maxResults) || !is_null($this->next_page_token)) {
             $endpoint .= '?' . http_build_query($this->query_string);
         }
+
         return $endpoint;
     }
 }
